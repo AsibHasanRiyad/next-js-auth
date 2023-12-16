58c43e5d49db87f9e021054e767fadd23b5832c4
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-12-16 15:51:45 +0600

    connect signup to the database

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,29 +1,139 @@
 "use client";
-
-import Image from "next/image";
+import axios from "axios";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { DNA } from "react-loader-spinner";
 
 const SignUpPage = () => {
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState("");
   const [user, setUser] = React.useState({
     name: "",
     email: "",
     password: "",
   });
-  const signUp = async () => {};
+  // const [buttonDisabled, setButtonDisabled] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+  const handelSubmit = async (e: any) => {
+    try {
+      setLoading(true);
+      e.preventDefault();
+      const form = e.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const password = form.password.value;
+      setErrorMessage("");
+      if (password.length < 6) {
+        setErrorMessage("Password should be more than 6 characters");
+        return;
+      } else if (!/[A-Z]/.test(password)) {
+        setErrorMessage("Your Password should have at least one Uppercase");
+        return;
+      } else if (!/[!@#$%^&*]/.test(password)) {
+        setErrorMessage(
+          "Your Password should have at least one special character - !@#$%^&*"
+        );
+        return;
+      } else if (!/[1-9]/.test(password)) {
+        setErrorMessage("Your Password should have at least one number");
+        return;
+      }
+      console.log(name, email, password);
+      const user = { name, email, password };
+      console.log(user);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("signUp Success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Sign up failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   if (
+  //     user.email.length > 0 &&
+  //     user.name.length > 0 &&
+  //     user.password.length > 0
+  //   ) {
+  //     setButtonDisabled(false);
+  //   } else {
+  //     setButtonDisabled(true);
+  //   }
+  // }, [user]);
   console.log(user);
   return (
-    <div className=" min-h-screen flex justify-center items-center">
-      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-        <div className="hidden bg-cover lg:block lg:w-1/2"></div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
+        <form onSubmit={handelSubmit} className="w-full max-w-md">
+          <h1 className="mt-3 text-2xl text-center font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white">
+            Register
+          </h1>
 
-        <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
-          <a
-            href="#"
-            className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
-          >
-            <div className="px-4 py-2">
-              <svg className="w-6 h-6" viewBox="0 0 40 40">
+          <div className="relative flex items-center mt-8">
+            <input
+              type="text"
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-4 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Name"
+              name="name"
+              required
+            />
+          </div>
+          <div className="relative flex items-center mt-8">
+            <input
+              type="email"
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-4 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Email address"
+              name="email"
+              required
+            />
+          </div>
+
+          <div className="relative flex items-center mt-4">
+            <input
+              type="password"
+              className="block w-full px-4 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Password"
+              name="password"
+              required
+            />
+          </div>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+            >
+              {loading ? (
+                <div className=" flex justify-center items-center">
+                  <DNA
+                    visible={true}
+                    height="30"
+                    width="30"
+                    ariaLabel="dna-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="dna-wrapper"
+                  />
+                </div>
+              ) : (
+                "Sign Up"
+              )}
+            </button>
+            <h1 className=" text-center text-red-500 py-2">{errorMessage}</h1>
+
+            <p className="mt-4 text-center text-gray-600 dark:text-gray-400">
+              or
+            </p>
+
+            <button
+              // onClick={handelGoogleSignIn}
+              className="flex items-center justify-center w-full px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
                 <path
                   d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
                   fill="#FFC107"
@@ -41,100 +151,19 @@ const SignUpPage = () => {
                   fill="#1976D2"
                 />
               </svg>
-            </div>
 
-            <span className="w-5/6 px-4 py-3 font-bold text-center">
-              Sign in with Google
-            </span>
-          </a>
-
-          <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
-
-            <a
-              href="#"
-              className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
-            >
-              or SignUp with email
-            </a>
-
-            <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
-          </div>
-
-          <div className="mt-4">
-            <label
-              htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-            >
-              Name
-            </label>
-            <input
-              value={user.name}
-              onChange={(e) => setUser({ ...user, name: e.target.value })}
-              id="LoggingEmailAddress"
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-              type="text"
-            />
-          </div>
-          <div className="mt-4">
-            <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
-              Email Address
-            </label>
-            <input
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              id="LoggingEmailAddress"
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-              type="email"
-            />
-          </div>
-
-          <div className="mt-4">
-            <div className="flex justify-between">
-              <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
-                Password
-              </label>
-              <a
-                href="#"
-                className="text-xs text-gray-500 dark:text-gray-300 hover:underline"
-              >
-                Forget Password?
-              </a>
-            </div>
-
-            <input
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              id="loggingPassword"
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-              type="password"
-            />
-          </div>
-
-          <div className="mt-6">
-            <button
-              onClick={signUp}
-              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
-            >
-              Sign Up
+              <span className="mx-2">Sign in with Google</span>
             </button>
+
+            <div className="mt-6 text-center ">
+              <div className="text-sm mb-4 text-blue-500 hover:underline dark:text-blue-400">
+                Already have an account ? <Link href={"/login"}>Sign In</Link>
+              </div>
+            </div>
           </div>
-
-          <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-
-            <Link
-              href="/login"
-              className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
-            >
-              or sign in
-            </Link>
-
-            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-          </div>
-        </div>
+        </form>
       </div>
-    </div>
+    </section>
   );
 };
 
