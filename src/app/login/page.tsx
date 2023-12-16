5d3f4a325facdb67c006fb7e8c0c749f0095cf4d
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-12-16 12:51:28 +0600

    inital setup done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className=" flex justify-center items-center min-h-screen text-5xl">
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className=" flex justify-center items-center min-h-screen text-5xl">
+        SignUp
+      </h1>
+    </div>
+  );
+};
+
+export default page;
