eb9b9c242f8313e12fcc7e783a8ea32b37c5f42e
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-12-16 19:09:20 +0600

    login functionality is implemented

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -34,7 +34,7 @@ export async function POST(request: NextRequest){
 
    return NextResponse.json({
     message:'User created successfully',
-    success:'true',
+    success:true,
     savedUser
    })
         
